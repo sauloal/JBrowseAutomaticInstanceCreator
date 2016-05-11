f488d0d438875124d15f40b051be42e68c3882a6
sauloal <sauloal@yahoo.com.br>
2016-05-11 17:11:14 +0200

    fixed bug in namig of files containing nfo

--- fix_perm.sh
+++ fix_perm.sh
@@ -4,5 +4,5 @@ set -xeu
 
 USER_UID=`id -u $USER`
 
-./run.sh chown -R -v ${USER_UID}:${USER_UID} /data
+./run.sh chown -R -v ${USER_UID}:${USER_UID} /data$@
 
--- test.sh
+++ test.sh
@@ -9,40 +9,50 @@ mkdir -p data/tom2_40/bam &>/dev/null || true
 
 
 
+FN=RF_001_SZAXPI008746-45.bam
 if [[ ! -f "data/tom2_40/bam/RF_001_SZAXPI008746-45.bam" ]]; then
-wget -O data/tom2_40/bam/RF_001_SZAXPI008746-45.bam.tmp ftp://ftp.sra.ebi.ac.uk/vol1/ERA282/ERA282888/bam/RF_001_SZAXPI008746-45.bam && mv data/tom2_40/bam/RF_001_SZAXPI008746-45.bam.tmp data/tom2_40/bam/RF_001_SZAXPI008746-45.bam
+wget -O data/tom2_40/bam/${FN}.tmp ftp://ftp.sra.ebi.ac.uk/vol1/ERA282/ERA282888/bam/${FN} && mv data/tom2_40/bam/${FN}.tmp data/tom2_40/bam/${FN}
 fi
 
-if [[ ! -f "data/tom2_40/bam/RF_001_SZAXPI008746-45.bam.bai" ]]; then
-samtools index data/tom2_40/bam/RF_001_SZAXPI008746-45.bam
-fi
-
-
-
-
-if [[ ! -f "data/tom2_40/vcf/RF_001_SZAXPI008746-45.vcf.gz" ]]; then
-wget -O data/tom2_40/vcf/RF_001_SZAXPI008746-45.vcf.gz.tmp ftp://ftp.sra.ebi.ac.uk/vol1/ERZ020/ERZ020447/RF_001_SZAXPI008746-45.vcf.gz && mv data/tom2_40/vcf/RF_001_SZAXPI008746-45.vcf.gz.tmp data/tom2_40/vcf/RF_001_SZAXPI008746-45.vcf.gz
-fi
-
-if [[ ! -f "data/tom2_40/vcf/RF_001_SZAXPI008746-45.vcf.gz.tbi" ]]; then
-
-gunzip -c data/tom2_40/vcf/RF_001_SZAXPI008746-45.vcf.gz | bgzip -c > data/tom2_40/vcf/RF_001_SZAXPI008746-45.vcf.tbz.tmp && mv data/tom2_40/vcf/RF_001_SZAXPI008746-45.vcf.tbz.tmp data/tom2_40/vcf/RF_001_SZAXPI008746-45.vcf.tbz
-
-rm data/tom2_40/vcf/RF_001_SZAXPI008746-45.vcf.gz
-
-mv data/tom2_40/vcf/RF_001_SZAXPI008746-45.vcf.tbz data/tom2_40/vcf/RF_001_SZAXPI008746-45.vcf.gz
-
-tabix data/tom2_40/vcf/RF_001_SZAXPI008746-45.vcf.gz
+if [[ ! -f "data/tom2_40/bam/${FN}.bai" ]]; then
+samtools index data/tom2_40/bam/${FN}
 fi
 
 
 
-if [[ ! -f "data/tom2_40/gff/ITAG2.3_assembly.gff3" ]]; then
-wget -O data/tom2_40/gff/ITAG2.3_assembly.gff3.tmp ftp://ftp.solgenomics.net/genomes/Solanum_lycopersicum/annotation/ITAG2.3_release/ITAG2.3_assembly.gff3 && mv data/tom2_40/gff/ITAG2.3_assembly.gff3.tmp data/tom2_40/gff/ITAG2.3_assembly.gff3
+FN=RF_001_SZAXPI008746-45.vcf
+FNZ=${FN}.gz
+if [[ ! -f "data/tom2_40/vcf/${FNZ}" ]]; then
+wget -O data/tom2_40/vcf/${FNZ}.tmp ftp://ftp.sra.ebi.ac.uk/vol1/ERZ020/ERZ020447/${FNZ} && mv data/tom2_40/vcf/${FNZ}.tmp data/tom2_40/vcf/${FNZ}
 fi
 
-if [[ ! -f "data/tom2_40/ref/S_lycopersicum_chromosomes.2.40.fa" ]]; then
-wget -O - ftp://ftp.solgenomics.net/genomes/Solanum_lycopersicum/assembly/build_2.40/S_lycopersicum_chromosomes.2.40.fa.gz | gunzip -c > data/tom2_40/ref/S_lycopersicum_chromosomes.2.40.fa.tmp && mv data/tom2_40/ref/S_lycopersicum_chromosomes.2.40.fa.tmp data/tom2_40/ref/S_lycopersicum_chromosomes.2.40.fa
+if [[ ! -f "data/tom2_40/vcf/${FNZ}.tbi" ]]; then
+FNT=${FN}.tbz
+gunzip -c data/tom2_40/vcf/${FNZ} | bgzip -c > data/tom2_40/vcf/${FNT}.tmp && mv data/tom2_40/vcf/${FNT}.tmp data/tom2_40/vcf/${FNT}
+
+rm data/tom2_40/vcf/${FNZ}
+
+mv data/tom2_40/vcf/${FNT} data/tom2_40/vcf/${FNZ}
+
+tabix data/tom2_40/vcf/${FNZ}
+fi
+
+
+FN=ITAG2.3_assembly.gff3
+if [[ ! -f "data/tom2_40/gff/${FN}" ]]; then
+wget -O data/tom2_40/gff/${FN}.tmp ftp://ftp.solgenomics.net/genomes/Solanum_lycopersicum/annotation/ITAG2.3_release/${FN} && mv data/tom2_40/gff/${FN}.tmp data/tom2_40/gff/${FN}
+fi
+
+FN=ITAG2.3_sgn_data.gff3
+if [[ ! -f "data/tom2_40/gff/${FN}" ]]; then
+mkdir -p data/tom2_40/gff/ITAG/ &>/dev/null || true
+wget -O data/tom2_40/gff/ITAG/${FN}.tmp ftp://ftp.solgenomics.net/genomes/Solanum_lycopersicum/annotation/ITAG2.3_release/${FN} && \
+mv data/tom2_40/gff/ITAG/${FN}.tmp data/tom2_40/gff/ITAG/${FN}
+fi
+
+FN=S_lycopersicum_chromosomes.2.40.fa
+if [[ ! -f "data/tom2_40/ref/${FN}" ]]; then
+wget -O - ftp://ftp.solgenomics.net/genomes/Solanum_lycopersicum/assembly/build_2.40/${FN}.gz | gunzip -c > data/tom2_40/ref/${FN}.tmp && mv data/tom2_40/ref/${FN}.tmp data/tom2_40/ref/${FN}
 fi
 
 
